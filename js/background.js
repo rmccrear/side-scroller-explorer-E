1ece4af06372ddc41cf31fb1392bfeb4059e2cec
Jo Ann <sweettroubles29@gmail.com>
2025-08-05 14:42:41 -0500

    Added Sun and Color

--- js/background.js
+++ js/background.js
@@ -4,7 +4,9 @@ function drawBackground() {
     background(135, 206, 235);
     
     // TODO: Sun on the right side
-    
+    fill(248, 248, 64);
+    ellipse(350, 50, 100, 100); 
+     
     // Grass at the bottom (100px high)
     fill(34, 139, 34);
     rect(0, 400 - 100, 400, 100);
