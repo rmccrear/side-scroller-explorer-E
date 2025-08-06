74e16075fa4e59d5e4f81b4ef1dc455d703f3a6d
Jarnell Williams <a.jwillz225@gmail.com.fr>
2025-08-05 21:20:23 -0500

    update

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -3,7 +3,7 @@ function doSpriteInteraction() {
     // Check if player touches food
     if (player.isTouching(food)) {
         score = score + 10; // Add 10 points
-        food.x = 400; // Send food back to start (right edge)
+        food.x = 450; // Send food back to start (right edge)
     }
     if (player.isTouching(enemy)) {
        health = health - 1; // Subtract 1 points
